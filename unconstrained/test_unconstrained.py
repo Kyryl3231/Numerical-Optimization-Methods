977f1dd852a55f3023638b0639dd25f37dde68f1
Kyryl Krynychnyi <mr.kkd22@gmail.com>
2022-08-29 23:52:08 +0200

    Implement Newton Method

--- unconstrained/test_unconstrained.py
+++ unconstrained/test_unconstrained.py
@@ -1,60 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import test_functions as tf
-from unconstrained import steepest_descent
+from unconstrained import steepest_descent, newton_method
 import sys
 sys.path.append("..")
 from utilities.gradient import gradient_ls
+from utilities.hessian import hessian_ls
 
 criteria = ["epsilon * |f(x_k)-f(x_k-1)| >= |f(x_k+1) - f(x_k)|",
             "epsilon ||x_k-x_k-1|| >= ||x_k+1 - x_k||",
             "epsilon >= ||gradient f(x_k)||", 
             "number of iterations"]
-            
-# TEST 1
-for fun,sol,name in zip(tf.test1_fun_list, tf.test1_solutions, tf.test1_fun_names):
-    print("Function: {}".format(name))
-    for x_0 in tf.test1_starting_points:
-        x_0 = np.longdouble(x_0)
-        minimizer, iter_count, stopping_criterion, stopping_values = steepest_descent(fun, x_0, max_iter=10**5)
-        dist_to_solution = min([np.linalg.norm(minimizer-x) for x in sol])
-        print("Minimizer x = [{:.5f}, {:.5f}] was found in {} iterations. Distance to solution: {:.6f}".format(minimizer[0], minimizer[1], iter_count, dist_to_solution)) 
+
+optimization_methods = [steepest_descent, newton_method]
+optimization_methods_names = ["The Steepest Descent", "Newton Method"]
+for opt_fun, opt_fun_name in zip(optimization_methods, optimization_methods_names):            
+    print(opt_fun_name)
+    # TEST 1
+    for fun,sol,name in zip(tf.test1_fun_list, tf.test1_solutions, tf.test1_fun_names):
+        print("Function: {}".format(name))
+        for x_0 in tf.test1_starting_points:
+            x_0 = np.longdouble(x_0)
+            minimizer, iter_count, stopping_criterion, stopping_values = opt_fun(fun, x_0, max_iter=10**5)
+            dist_to_solution = min([np.linalg.norm(minimizer-x) for x in sol])
+            print("Minimizer x = [{:.5f}, {:.5f}] was found in {} iterations. Distance to solution: {:.6f}".format(minimizer[0], minimizer[1], iter_count, dist_to_solution)) 
+            print("Stopping criterion: {}".format(criteria[stopping_criterion]))
+            for i in range(len(criteria)):
+                print(criteria[i]," = ",stopping_values[i])
+            print()
+
+    # TEST 2
+    plot_num = len(tf.test2_param_list)
+    fig = plt.figure(figsize=(10,5))
+    for plot_idx in range(plot_num):
+        # a = np.random.random(size=m)*2*q - q # points on [-q, q]
+        # a.sort()
+        q, m, max_degree = tf.test2_param_list[plot_idx]
+        a = np.linspace(-q,q,m)
+        b = np.sin(a)
+        x_0 = np.zeros(max_degree+1, dtype=np.longdouble)
+        plt.subplot(1, plot_num, plot_idx+1)
+        plt.plot(a,b, 'o-', label="original data")
+        if opt_fun == newton_method:
+            minimizer, it_num, stopping_criterion, stopping_values = opt_fun(tf.least_squares, x_0, a, b, gradient_fun=gradient_ls, hessian_fun=hessian_ls, max_iter=10**6)
+        else:
+            minimizer, it_num, stopping_criterion, stopping_values = opt_fun(tf.least_squares, x_0, a, b, gradient_fun=gradient_ls, max_iter=10**6)
+        print("Result: {}".format(minimizer))
+        print("Number of iterations: {}".format(it_num))
         print("Stopping criterion: {}".format(criteria[stopping_criterion]))
         for i in range(len(criteria)):
             print(criteria[i]," = ",stopping_values[i])
-        print()
-
-# TEST 2
-plot_num = len(tf.test2_param_list)
-fig = plt.figure(figsize=(10,5))
-for plot_idx in range(plot_num):
-    # a = np.random.random(size=m)*2*q - q # points on [-q, q]
-    # a.sort()
-    q, m, max_degree = tf.test2_param_list[plot_idx]
-    a = np.linspace(-q,q,m)
-    b = np.sin(a)
-    x_0 = np.zeros(max_degree+1, dtype=np.longdouble)
-    plt.subplot(1, plot_num, plot_idx+1)
-    plt.plot(a,b, 'o-', label="original data")
-    minimizer, it_num, stopping_criterion, stopping_values = steepest_descent(tf.least_squares, x_0, a, b, gradient_fun=gradient_ls, max_iter=10**6)
-    print("Result: {}".format(minimizer))
-    print("Number of iterations: {}".format(it_num))
-    print("Stopping criterion: {}".format(criteria[stopping_criterion]))
-    for i in range(len(criteria)):
-        print(criteria[i]," = ",stopping_values[i])
-    print("Function to approximate: g(x) = sin(x)")
-    print("Maximal degree of approximation: {}".format(max_degree))
-    pol_str = " + ".join(["{:.5f}*x^{}".format(minimizer[i],i) for i in range(len(minimizer))])
-    print("Final optimal polynomial:")
-    print(pol_str, '\n')
-    d = np.array([tf.phi(minimizer, el) for el in a]) # approximation
-    taylor = np.array([tf.taylor_expansion_sin(el,max_degree) for el in a])
-    plt.plot(a,d, 'o-', label="approximation")
-    plt.plot(a,taylor, 'o-', label="taylor expansion")
-    plt.xlabel("argument value")
-    plt.ylabel("sin() value")
-    plt.ylim(-1,1)
-    plt.title("[-{}, {}] with {} points and max_degree = {}".format(q,q,m,max_degree))
-    plt.legend()
-plt.show()
-fig.savefig('test_plots')
+        print("Function to approximate: g(x) = sin(x)")
+        print("Maximal degree of approximation: {}".format(max_degree))
+        pol_str = " + ".join(["{:.5f}*x^{}".format(minimizer[i],i) for i in range(len(minimizer))])
+        print("Final optimal polynomial:")
+        print(pol_str, '\n')
+        d = np.array([tf.phi(minimizer, el) for el in a]) # approximation
+        taylor = np.array([tf.taylor_expansion_sin(el,max_degree) for el in a])
+        plt.plot(a,d, 'o-', label="approximation")
+        plt.plot(a,taylor, 'o-', label="taylor expansion")
+        plt.xlabel("argument value")
+        plt.ylabel("sin() value")
+        plt.ylim(-1,1)
+        plt.title("[-{}, {}] with {} points and max_degree = {}".format(q,q,m,max_degree))
+        plt.legend()
+    plt.show()
+    fig.savefig('test_plots')
--- unconstrained/unconstrained.py
+++ unconstrained/unconstrained.py
@@ -1,9 +1,11 @@
 import numpy as np
 from typing import Callable
+import scipy
 import sys
 sys.path.append("..")
 
 from utilities.gradient import gradient
+from utilities.hessian import hessian
 from utilities.step_length import line_search_alpha
 
 
@@ -101,3 +103,136 @@ def steepest_descent(obj_fun: Callable, x_0: np.ndarray, *args, **kwargs) -> np.
 
     return x, iter, stopping_criterion, stopping_values
 
+def cholesky_modified(A: np.ndarray, beta=np.longdouble(1e-3), lower=False):
+    """
+    Modified Cholesky factorization with added multiple of identity
+
+    Params
+    ----------
+    A : numpy.ndarray
+        matrix to decompose
+    beta : np.longdouble
+        modification parameter
+    lower : bool
+        Whether to compute the upper- or lower-triangular Cholesky factorization
+    
+    Return
+    ----------
+    L : numpy.ndarray
+        Cholesky factor 
+    """
+
+    el_num = len(A)
+    min_diag_el = min([A[i,i] for i in range(el_num)])
+    if min_diag_el > 0:
+        tau = np.longdouble(0)
+    else:
+        tau = -min_diag_el + beta
+    
+    while True:
+        try: 
+            L = scipy.linalg.cholesky(A+tau*np.identity(el_num), lower=lower)
+            break
+        except scipy.linalg.LinAlgError:
+            tau = max(2*tau, beta)
+    return L
+
+def newton_method(obj_fun: Callable, x_0: np.ndarray, *args, **kwargs) -> np.ndarray:
+    """
+    The Newton Method optimization
+
+    Params
+    ----------
+    obj_fun : function
+        objective function to minimize
+    x_0 : numpy.ndarray
+        a vector representing initial point
+    *args: 
+        additional arguments for obj_fun
+
+    Additional params
+    ----------
+    max_iter : int 
+        maximum number of iterations
+    tol : float
+        tolerance for the solution
+    gradient_fun : function
+        implementation for gradient estimation
+    hessian_fun: function
+        implementation for hessian estimation
+        
+    Return
+    ----------
+    x : numpy.longdouble or numpy.ndarray
+        minimizer of the objective function
+    iter : int
+        iteration counter
+    stopping_criterion : int
+        stopping criterion index
+    stopping_values : numpy.ndarray
+        values of stopping criterions
+    """
+    
+    max_iter = kwargs.get('max_iter',1000)
+    tol = kwargs.get('tol',np.longdouble(1e-5))
+    gradient_fun = kwargs.get('gradient_fun',gradient)
+    hessian_fun = kwargs.get('hessian_fun',hessian)
+
+    if isinstance(x_0,np.longdouble):
+        dim_num = 1
+    elif isinstance(x_0, np.ndarray):
+        if len(x_0.shape) != 1:
+            raise ValueError("x_0 should have only 1 dimension, got {}".format(len(x_0.shape)))
+        if isinstance(x_0[0],np.longdouble):
+            dim_num = len(x_0)
+        else:
+            raise TypeError("Elements of x_0 should be of type np.longdouble, got {}".format(type(x_0[0])))
+    else:
+        raise TypeError("x_0 should be of type numpy.longdouble or numpy.ndarray, got {}".format(type(x_0)))
+
+    x = np.longdouble(x_0)
+    grad = gradient_fun(obj_fun, x, *args)
+    iter = 0
+    stopping_values = np.zeros(4)
+    to_stop=False
+    while not to_stop:
+        grad = gradient_fun(obj_fun, x, *args)
+        hess = hessian_fun(obj_fun, x, *args)
+        if dim_num==1:
+            p = -hess**(-1)*grad
+        else:
+            # get p by solving hess @ p = -grad 
+            # try:
+            # cholesky decomposition of the Hessian
+            L = cholesky_modified(hess, lower=True)
+            # solve L @ L.T @ p = -grad with L.T @ p = z
+            z = scipy.linalg.solve(L,-grad, lower=True)
+            p = scipy.linalg.solve(L.T, z, lower=False)
+            # except scipy.linalg.LinAlgError:
+                # p = -grad
+                # print("grad was used")
+        alpha = line_search_alpha(obj_fun, x, p, grad, *args) 
+        iter += 1
+        stopping_values[2] = np.linalg.norm(grad)
+        stopping_values[3] = iter
+        # update x
+        x_new = x + alpha*p
+        fun_value_step_new = np.abs(obj_fun(x_new, *args)-obj_fun(x, *args))
+        arg_step_new = np.linalg.norm(x_new-x)
+        x = x_new 
+        # check stopping criteria
+        if fun_value_step_new <= tol*stopping_values[0]:
+            stopping_criterion = 0
+            to_stop = True
+        if arg_step_new <= tol*stopping_values[1]:
+            stopping_criterion = 1
+            to_stop = True
+        if stopping_values[2] <= tol:
+            stopping_criterion = 2
+            to_stop = True
+        if iter>=max_iter:
+            stopping_criterion = 3
+            to_stop = True    
+        stopping_values[0] = fun_value_step_new
+        stopping_values[1] = arg_step_new
+    return x, iter, stopping_criterion, stopping_values
